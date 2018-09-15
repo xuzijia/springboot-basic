815374ebca800251c3183ed72a2a0370c78c3b89
simple <2295443695@qq.com>
2018-09-15 11:32:53 +0800

    springboot 初始化版本

---
+++ src/main/java/com/simple/springbootbasic/SpringbootBasicApplication.java
@@ -0,0 +1,12 @@
+package com.simple.springbootbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBasicApplication.class, args);
+    }
+}
