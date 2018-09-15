e91348b5ce9fca57e642a283e3f43364fabf1dff
simple <2295443695@qq.com>
2018-09-15 11:59:54 +0800

    更改项目端口

--- src/main/java/com/simple/springbootbasic/SpringbootBasicApplication.java
+++ src/main/java/com/simple/springbootbasic/SpringbootBasicApplication.java
@@ -3,6 +3,9 @@ package com.simple.springbootbasic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 程序启动入口
+ */
 @SpringBootApplication
 public class SpringbootBasicApplication {
 
